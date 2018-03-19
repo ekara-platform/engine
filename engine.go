68974c56c555230df604ce0983544d7f595d9d8a
nirekin <nirekin@hotmail.com>
2018-03-19 12:50:33 +0100

    Add descriptor serialization

--- engine.go
+++ engine.go
@@ -2,6 +2,8 @@ package engine
 
 import (
 	"log"
+
+	"gopkg.in/yaml.v2"
 )
 
 //go:generate go run ./generator/generate.go
@@ -25,4 +27,10 @@ func Create(logger *log.Logger, location string) (lagoon Lagoon, err error) {
 	}
 	h.env = &desc
 	return &h, nil
+}
+
+//GetContent serializes the content on the environment descriptor
+func (h holder) GetContent() ([]byte, error) {
+	b, e := yaml.Marshal(h.env)
+	return b, e
 }
