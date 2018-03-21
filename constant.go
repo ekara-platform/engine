70da43d08758141edd3276fc57d3252083e46816
nirekin <nirekin@hotmail.com>
2018-03-21 09:41:23 +0100

    Add constant file

---
+++ constant.go
@@ -0,0 +1,8 @@
+package engine
+
+const (
+
+	// The environment variable used to pass the environment descriptor
+	// content between components.
+	starterEnvVariableKey string = "LAGOON_ENV_DESCR"
+)
