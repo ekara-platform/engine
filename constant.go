f261507096496e6b5ab399d0d94740d53eb17b31
Guillaume Barré <nirekin@hotmail.com>
2018-05-23 16:47:47 +0200

    Added methods to deal with file and session (#11)
    * Add common methodsto deal with file and session
    
    * Added common methods to deal with session

--- constant.go
+++ constant.go
@@ -1,9 +1,48 @@
 package engine
 
 const (
+	// The name of the environment file descriptor
+	//
+	// All requests of environment creation or update are made passing as parameter
+	// the root folder of the environment specification. Then the Laggon Engine
+	// will look for this specific file into the given folder.
 	DescriptorFileName string = "lagoon.yaml"
 
 	// The environment variable used to pass the environment descriptor
-	// location between components.
+	// location between several Lagoon components.
 	StarterEnvVariableKey string = "LAGOON_ENV_DESCR"
+
+	// The environment variable used to tell the Lagoon Engine if we are runnig
+	// a "creation" or an "update"
+	StarterCreateEnvVariableKey string = "LAGOON_CREATE"
+
+	// The name of the client requesting the environment creation or update
+	ClientEnvVariableKey string = "LAGOON_CLIENT"
+
+	// The environment variable used to get or store the http proxy definition
+	HttpProxyEnvVariableKey string = "http_proxy"
+
+	// The environment variable used to get or store the https proxy definition
+	HttpsProxyEnvVariableKey string = "https_proxy"
+
+	// The environment variable used to get or store the no proxy definition
+	NoProxyEnvVariableKey string = "no_proxy"
+
+	// The name of the generated proxy configuration file
+	ProxyConfigFileName string = "proxy_env.yml"
+
+	// The name of the generated configuration file for a node
+	NodeConfigFileName string = "conf.yml"
+
+	// The name of the generated docker config file for a node
+	NodeDockerFileName string = "docker.yml"
+
+	// The name of the generated creation session file returned to the client
+	CreationSessionFileName string = "create_session.json"
+
+	// Prefix for the installer logs
+	InstallerLogPrefix string = "Lagoon INSTALLER LOG:"
+
+	// Volum location used by the installer
+	InstallerVolume string = "/opt/lagoon/output"
 )
--- engine.go
+++ engine.go
@@ -1,12 +1,19 @@
 package engine
 
 import (
+	"fmt"
+	"hash/crc64"
 	"log"
+	"net"
 	"net/url"
+	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/lagoon-platform/model"
+	"gopkg.in/yaml.v2"
 )
 
 type Lagoon interface {
@@ -92,4 +99,113 @@ func (c *context) Environment() model.Environment {
 
 func (c *context) ComponentManager() ComponentManager {
 	return c.componentManager
+}
+
+func GetUId() string {
+	sIp := getOutboundIP().String()
+	sTime := time.Now().UTC().String()
+	s := sIp + sTime
+	aStringToHash := []byte(s)
+	crc64Table := crc64.MakeTable(0xC96C5795D7870F42)
+	crc64Int := crc64.Checksum(aStringToHash, crc64Table)
+	return strconv.FormatUint(crc64Int, 16)
+}
+
+func getOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP
+}
+
+//SaveFile saves the given bytes into a fresh new file specified by its folder
+//and name.
+//
+//If the file already exists then it will be replaced.
+func SaveFile(logger *log.Logger, folder string, name string, b []byte) error {
+	l := filepath.Join(folder, name)
+	logger.Printf(LOG_SAVING, l)
+	os.Remove(l)
+	if _, err := os.Stat(name); os.IsNotExist(err) {
+		e := os.MkdirAll(folder, 0700)
+		if e != nil {
+			logger.Printf(ERROR, e.Error())
+			return e
+		}
+
+		logger.Printf(LOG_CREATING_CONFIG_FILE, l)
+
+		f, e := os.Create(l)
+		if e != nil {
+			logger.Printf(ERROR, e.Error())
+			return fmt.Errorf(ERROR_CREATING_CONFIG_FILE, name, e.Error())
+		}
+		defer f.Close()
+		_, e = f.Write(b)
+		if e != nil {
+			return e
+		}
+	}
+	return nil
+}
+
+// Proxy describes the structure used to Marshal the content of the proxy file configuration
+type Proxy struct {
+	// The root of the proxy specification
+	ProxyEnv      ProxyEnv `yaml:"proxy_env"`
+	ProxyHost     string   `yaml:"proxy_host"`
+	ProxyPort     string   `yaml:"proxy_port"`
+	ProxyUser     string   `yaml:"proxy_user"`
+	ProxyPassword string   `yaml:"proxy_password"`
+}
+
+// ProxyEnv contains the proxy file configuration
+type ProxyEnv struct {
+	// The "HTTP PROXY" specification
+	Http string `yaml:"http_proxy"`
+	// The "HTTPS PROXY" specification
+	Https string `yaml:"https_proxy"`
+	// The "NO PROXY" specification
+	No string `yaml:"no_proxy"`
+}
+
+//proxyConfig returns the content of the proxy configuration file
+func proxyConfig(http string, https string, no string) (b []byte, e error) {
+	proxy := Proxy{ProxyEnv: ProxyEnv{Http: http, Https: https, No: no}, ProxyHost: "http.internetpsa.inetpsa.com", ProxyPort: "80", ProxyUser: "mzplagww", ProxyPassword: "wwlag00n"}
+	b, e = yaml.Marshal(&proxy)
+	return
+}
+
+// SaveProxy creates and saves the proxy configuration file.
+//
+// The file will be saved into the given folder with the name:
+//  engine.ProxyConfigFileName
+func SaveProxy(logger *log.Logger, folder string, httpProxy string, httpsProxy string, noProxy string) error {
+	b, e := proxyConfig(httpProxy, httpsProxy, noProxy)
+	if e != nil {
+		logger.Printf(ERROR_GENERATING_PROXY_CONFIG, e.Error())
+		return e
+	}
+
+	e = SaveFile(logger, folder, ProxyConfigFileName, b)
+	if e != nil {
+		return e
+	}
+	return nil
+}
+
+//CheckProxy returns the proxy setting from environment variables
+//
+// See:
+//		engine.HttpProxyEnvVariableKey
+//		engine.HttpsProxyEnvVariableKey
+//		engine.NoProxyEnvVariableKey
+func CheckProxy() (httpProxy string, httpsProxy string, noProxy string) {
+	httpProxy = os.Getenv(HttpProxyEnvVariableKey)
+	httpsProxy = os.Getenv(HttpsProxyEnvVariableKey)
+	noProxy = os.Getenv(NoProxyEnvVariableKey)
+	return
 }
