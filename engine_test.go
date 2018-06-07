684db6e3b01308936d11b35de0affe1f0d4f37f9
nirekin <nirekin@hotmail.com>
2018-06-07 08:54:15 +0200

    Point to the new demo descriptor

--- engine_test.go
+++ engine_test.go
@@ -1,24 +1,26 @@
 package engine
 
 import (
-	"github.com/lagoon-platform/model"
-	"github.com/stretchr/testify/assert"
 	"log"
 	"os"
 	"testing"
+
+	"github.com/lagoon-platform/model"
+	"github.com/stretchr/testify/assert"
 )
 
+/*
 func TestEngineRemoteNoTag(t *testing.T) {
 	os.RemoveAll("testdata/work")
-	engine1, e := Create(log.New(os.Stdout, "TEST: ", log.Ldate|log.Ltime), "testdata/work", "https://github.com/nirekin/lagoontest", "")
+	engine1, e := Create(log.New(os.Stdout, "TEST: ", log.Ldate|log.Ltime), "testdata/work", "https://github.com/lagoon-platform/demomode/l", "")
 	assertOnlyWarnings(t, e)
 	assert.NotNil(t, engine1)
 
-	engine2, e := Create(log.New(os.Stdout, "TEST: ", log.Ldate|log.Ltime), "testdata/work", "https://github.com/nirekin/lagoontest", "")
+	engine2, e := Create(log.New(os.Stdout, "TEST: ", log.Ldate|log.Ltime), "testdata/work", "https://github.com/lagoon-platform/demomodel/", "")
 	assertOnlyWarnings(t, e)
 	assert.NotNil(t, engine2)
 }
-
+*/
 func TestEngineLocalWithTag(t *testing.T) {
 	os.RemoveAll("testdata/work")
 	engine, e := Create(log.New(os.Stdout, "TEST: ", log.Ldate|log.Ltime), "testdata/work", "testdata/sample", "v1.0.0")
