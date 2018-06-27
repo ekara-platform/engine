f895b0fcf100a60309601531e78d779cce2ebfb5
nirekin <nirekin@hotmail.com>
2018-06-27 12:30:07 +0200

    Tests on ExchangeFolders

--- folder.go
+++ folder.go
@@ -13,34 +13,6 @@ import (
 )
 
 // FolderPath represents a folder used into the ExchangeFolder structure
-type FolderPath interface {
-	//Path returns the path to this folder
-	Path() string
-
-	//AdaptedPath returns the path of this folder adapted depending
-	// on the OS we are running on
-	// For example on Windows it convert c:\Users\blablabla
-	// to /c/Users/blablabla
-	AdaptedPath() string
-
-	// Exixts returns true if this folder exixts physically
-	Exixts() bool
-
-	// Contains returns true if this folder contains the given content
-	Contains(s string) bool
-
-	// ContainsParamYaml returns true if this folder contains a file named ParamYamlFileName
-	ContainsParamYaml() (ok bool, b []byte, e error)
-
-	// ContainsExtraVarsYaml returns true if this folder contains a file named ExtraVarYamlFileName
-	ContainsExtraVarsYaml() (ok bool, b []byte, e error)
-
-	// ContainsEnvYaml returns true if this folder contains a file named EnvYamlFileName
-	ContainsEnvYaml() (ok bool, b []byte, e error)
-
-	// Copy copies, if it exists, the content to the destination foler
-	Copy(content string, destination FolderPath) error
-}
 
 // ExchangeFolder represents a folder structure used to pass and get content to container.
 type ExchangeFolder struct {
@@ -55,23 +27,17 @@ type ExchangeFolder struct {
 // Create creates physically the ExchangeFolder.
 // If the ExchangeFolder already exist  it will remain untouched
 func (f ExchangeFolder) Create() error {
-	if _, err := os.Stat(f.Location.Path()); os.IsNotExist(err) {
-		e := os.Mkdir(f.Location.Path(), 0700)
-		if e != nil {
-			return e
-		}
+	e := f.Location.create()
+	if e != nil {
+		return e
 	}
-	if _, err := os.Stat(f.Input.Path()); os.IsNotExist(err) {
-		e := os.Mkdir(f.Input.Path(), 0700)
-		if e != nil {
-			return e
-		}
+	e = f.Input.create()
+	if e != nil {
+		return e
 	}
-	if _, err := os.Stat(f.Output.Path()); os.IsNotExist(err) {
-		e := os.Mkdir(f.Output.Path(), 0700)
-		if e != nil {
-			return e
-		}
+	e = f.Output.create()
+	if e != nil {
+		return e
 	}
 	return nil
 }
@@ -86,42 +52,68 @@ func (f ExchangeFolder) CleanAll() error {
 	return nil
 }
 
-func (f Folder) AdaptedPath() string {
+// Delete deletes the ExchangeFolder
+func (f ExchangeFolder) Delete() error {
+	e := os.RemoveAll(f.Location.Path())
+	if e != nil {
+		return e
+	}
+	return nil
+}
+
+//AdaptedPath returns the path of this folder adapted depending
+// on the OS we are running on
+// For example on Windows it convert c:\Users\blablabla
+// to /c/Users/blablabla
+func (f FolderPath) AdaptedPath() string {
 	return AdaptPath(f.path)
 }
 
-func (f Folder) Path() string {
+//Path returns the path to this folder
+func (f FolderPath) Path() string {
 	return f.path
 }
 
-func (f Folder) Exixts() bool {
+// Exixts returns true if this folder exixts physically
+func (f FolderPath) Exixts() (bool, error) {
 	_, err := os.Stat(f.path)
-	return os.IsNotExist(err)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
 }
 
-func (f Folder) Contains(s string) bool {
+// Contains returns true if this folder contains the given content
+func (f FolderPath) Contains(s string) bool {
 	if _, err := os.Stat(JoinPaths(f.Path(), s)); os.IsNotExist(err) {
 		return false
 	}
 	return true
 }
 
-func (f Folder) ContainsParamYaml() (ok bool, b []byte, e error) {
+// ContainsParamYaml returns true if this folder contains a file named ParamYamlFileName
+func (f FolderPath) ContainsParamYaml() (ok bool, b []byte, e error) {
 	ok, b, e = containsAndRead(f, ParamYamlFileName)
 	return
 }
 
-func (f Folder) ContainsExtraVarsYaml() (ok bool, b []byte, e error) {
+// ContainsExtraVarsYaml returns true if this folder contains a file named ExtraVarYamlFileName
+func (f FolderPath) ContainsExtraVarsYaml() (ok bool, b []byte, e error) {
 	ok, b, e = containsAndRead(f, ExtraVarYamlFileName)
 	return
 }
 
-func (f Folder) ContainsEnvYaml() (ok bool, b []byte, e error) {
+// ContainsEnvYaml returns true if this folder contains a file named EnvYamlFileName
+func (f FolderPath) ContainsEnvYaml() (ok bool, b []byte, e error) {
 	ok, b, e = containsAndRead(f, EnvYamlFileName)
 	return
 }
 
-func (f Folder) Copy(content string, destination FolderPath) error {
+// Copy copies, if it exists, the content to the destination foler
+func (f FolderPath) Copy(content string, destination FolderPath) error {
 	if f.Contains(content) {
 		in, err := os.Open(JoinPaths(f.Path(), content))
 		if err != nil {
@@ -145,8 +137,55 @@ func (f Folder) Copy(content string, destination FolderPath) error {
 	}
 }
 
-type Folder struct {
-	path string
+// AddChildExchangeFolder creates a child of type ExchangeFolder having
+// this folder as root location
+func (f *FolderPath) AddChildExchangeFolder(childName string) (ExchangeFolder, error) {
+	child, err := CreateExchangeFolder(f.Path(), childName)
+	if err != nil {
+		return ExchangeFolder{}, err
+	}
+	f.Children[childName] = child
+	return child, nil
+}
+
+// Write writes the given bytes into file within this folder
+func (f FolderPath) Write(bytes []byte, fileName string) error {
+	if ok, e := f.Exixts(); ok {
+		if e != nil {
+			return e
+		}
+		e = ioutil.WriteFile(JoinPaths(f.Path(), fileName), bytes, 0644)
+		if e != nil {
+			return e
+		}
+	} else {
+		return fmt.Errorf("The directory is not created")
+	}
+	return nil
+}
+
+// AddChildExchangeFolder creates a child of type ExchangeFolder having
+// this folder as root location
+func (f FolderPath) create() error {
+	if _, err := os.Stat(f.Path()); os.IsNotExist(err) {
+		e := os.Mkdir(f.Path(), 0700)
+		if e != nil {
+			return e
+		}
+	}
+
+	for _, v := range f.Children {
+		e := v.Create()
+		if e != nil {
+			return e
+		}
+	}
+	return nil
+}
+
+type FolderPath struct {
+	path     string
+	Children map[string]ExchangeFolder
 }
 
 func CreateExchangeFolder(location string, folderName string) (ExchangeFolder, error) {
@@ -155,9 +194,10 @@ func CreateExchangeFolder(location string, folderName string) (ExchangeFolder, e
 	if err != nil {
 		return r, err
 	}
-	r.Location = Folder{path: JoinPaths(outputDir, folderName)}
-	r.Output = Folder{path: JoinPaths(outputDir, folderName, "output")}
-	r.Input = Folder{path: JoinPaths(outputDir, folderName, "input")}
+	r.Location = FolderPath{path: JoinPaths(outputDir, folderName), Children: make(map[string]ExchangeFolder)}
+	r.Output = FolderPath{path: JoinPaths(outputDir, folderName, "output"), Children: make(map[string]ExchangeFolder)}
+	r.Input = FolderPath{path: JoinPaths(outputDir, folderName, "input"), Children: make(map[string]ExchangeFolder)}
+
 	return r, nil
 }
 
@@ -182,7 +222,7 @@ func AdaptPath(path string) string {
 	return s
 }
 
-func containsAndRead(f Folder, s string) (bool, []byte, error) {
+func containsAndRead(f FolderPath, s string) (bool, []byte, error) {
 	if ok := f.Contains(s); ok {
 		b, err := ioutil.ReadFile(path.Join(f.Path(), s))
 		if err != nil {
---
+++ folder_test.go
@@ -0,0 +1,153 @@
+package engine
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExchangeFolder(t *testing.T) {
+	ef, err := CreateExchangeFolder(".", "testExFolder")
+	assert.Nil(t, err)
+
+	ok, err := ef.Location.Exixts()
+	assert.False(t, ok)
+
+	ok, err = ef.Input.Exixts()
+	assert.False(t, ok)
+
+	ok, err = ef.Output.Exixts()
+	assert.False(t, ok)
+
+	// Physical creation
+	err = ef.Create()
+	assert.Nil(t, err)
+
+	ok, err = ef.Location.Exixts()
+	assert.True(t, ok)
+
+	ok, err = ef.Input.Exixts()
+	assert.True(t, ok)
+
+	ok, err = ef.Output.Exixts()
+	assert.True(t, ok)
+
+	// Deletion
+	err = ef.Delete()
+	assert.Nil(t, err)
+
+	ok, err = ef.Location.Exixts()
+	assert.False(t, ok)
+
+	ok, err = ef.Input.Exixts()
+	assert.False(t, ok)
+
+	ok, err = ef.Output.Exixts()
+	assert.False(t, ok)
+}
+
+func TestChildren(t *testing.T) {
+	ef, err := CreateExchangeFolder(".", "testExFolder")
+	assert.Nil(t, err)
+	child, err := ef.Input.AddChildExchangeFolder("child")
+	assert.Nil(t, err)
+
+	ok, err := child.Location.Exixts()
+	assert.False(t, ok)
+
+	ok, err = child.Input.Exixts()
+	assert.False(t, ok)
+
+	ok, err = child.Output.Exixts()
+	assert.False(t, ok)
+
+	// Physical creation of the parent
+	// This is supposed to also create the children
+	err = ef.Create()
+
+	ok, err = child.Location.Exixts()
+	assert.True(t, ok)
+
+	ok, err = child.Input.Exixts()
+	assert.True(t, ok)
+
+	ok, err = child.Output.Exixts()
+	assert.True(t, ok)
+
+	// Deletion of the child only
+	err = child.Delete()
+	assert.Nil(t, err)
+
+	ok, err = child.Location.Exixts()
+	assert.False(t, ok)
+
+	ok, err = child.Input.Exixts()
+	assert.False(t, ok)
+
+	ok, err = child.Output.Exixts()
+	assert.False(t, ok)
+
+	// we just recreate the child
+	err = ef.Create()
+
+	ok, err = child.Location.Exixts()
+	assert.True(t, ok)
+
+	ok, err = child.Input.Exixts()
+	assert.True(t, ok)
+
+	ok, err = child.Output.Exixts()
+	assert.True(t, ok)
+
+	// Physical creation of the parent
+	// This is supposed to also delete the children
+	err = ef.Delete()
+	assert.Nil(t, err)
+
+	ok, err = child.Location.Exixts()
+	assert.False(t, ok)
+
+	ok, err = child.Input.Exixts()
+	assert.False(t, ok)
+
+	ok, err = child.Output.Exixts()
+	assert.False(t, ok)
+
+}
+
+func TestContent(t *testing.T) {
+	ef, err := CreateExchangeFolder(".", "testExFolder")
+	assert.Nil(t, err)
+	defer ef.Delete()
+
+	b := []byte("Dummy file Content \n")
+	err = ef.Input.Write(b, "test.txt")
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "The directory is not created")
+
+	ef.Create()
+	err = ef.Input.Write(b, "test.txt")
+	assert.Nil(t, err)
+
+	ok := ef.Input.Contains("test.txt")
+	assert.True(t, ok)
+
+	ok = ef.Output.Contains("test.txt")
+	assert.False(t, ok)
+
+	err = ef.Input.Copy("test.txt", ef.Output)
+	assert.Nil(t, err)
+
+	ok = ef.Output.Contains("test.txt")
+	assert.True(t, ok)
+
+	err = ef.CleanAll()
+	assert.Nil(t, err)
+
+	ok = ef.Input.Contains("test.txt")
+	assert.False(t, ok)
+
+	ok = ef.Output.Contains("test.txt")
+	assert.False(t, ok)
+
+}
