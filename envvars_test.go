9987b3ba60e5c164e981f34a69038ad108bb1ae5
nirekin <nirekin@hotmail.com>
2018-07-13 16:36:32 +0200

    Add test for envvars

---
+++ envvars_test.go
@@ -0,0 +1,45 @@
+package engine
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUniqueEnvVars(t *testing.T) {
+	ev := BuildEnvVars()
+	assert.Equal(t, 0, len(ev.content))
+	ev.Add("key1", "value1")
+	assert.Equal(t, 1, len(ev.content))
+
+	val, ok := ev.content["key1"]
+	assert.True(t, ok)
+	assert.Equal(t, val, "value1")
+
+	ev.Add("key2", "value2")
+	assert.Equal(t, 2, len(ev.content))
+
+	val, ok = ev.content["key2"]
+	assert.True(t, ok)
+	assert.Equal(t, val, "value2")
+}
+
+func TestBufferedEnvVars(t *testing.T) {
+	ev := BuildEnvVars()
+	assert.Equal(t, 0, len(ev.content))
+
+	b := CreateBuffer()
+	b.Envvars["key1"] = "value1"
+	b.Envvars["key2"] = "value2"
+
+	ev.AddBuffer(b)
+	assert.Equal(t, 2, len(ev.content))
+
+	val, ok := ev.content["key1"]
+	assert.True(t, ok)
+	assert.Equal(t, val, "value1")
+
+	val, ok = ev.content["key2"]
+	assert.True(t, ok)
+	assert.Equal(t, val, "value2")
+}
