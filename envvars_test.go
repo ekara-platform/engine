d557575a73870b00f47f85df28152d8a057df031
nirekin <nirekin@hotmail.com>
2018-09-12 12:58:22 +0200

    Fix tests

--- envvars_test.go
+++ envvars_test.go
@@ -8,38 +8,38 @@ import (
 
 func TestUniqueEnvVars(t *testing.T) {
 	ev := BuildEnvVars()
-	assert.Equal(t, 0, len(ev.content))
+	assert.Equal(t, 0, len(ev.Content))
 	ev.Add("key1", "value1")
-	assert.Equal(t, 1, len(ev.content))
+	assert.Equal(t, 1, len(ev.Content))
 
-	val, ok := ev.content["key1"]
+	val, ok := ev.Content["key1"]
 	assert.True(t, ok)
 	assert.Equal(t, val, "value1")
 
 	ev.Add("key2", "value2")
-	assert.Equal(t, 2, len(ev.content))
+	assert.Equal(t, 2, len(ev.Content))
 
-	val, ok = ev.content["key2"]
+	val, ok = ev.Content["key2"]
 	assert.True(t, ok)
 	assert.Equal(t, val, "value2")
 }
 
 func TestBufferedEnvVars(t *testing.T) {
 	ev := BuildEnvVars()
-	assert.Equal(t, 0, len(ev.content))
+	assert.Equal(t, 0, len(ev.Content))
 
 	b := CreateBuffer()
 	b.Envvars["key1"] = "value1"
 	b.Envvars["key2"] = "value2"
 
 	ev.AddBuffer(b)
-	assert.Equal(t, 2, len(ev.content))
+	assert.Equal(t, 2, len(ev.Content))
 
-	val, ok := ev.content["key1"]
+	val, ok := ev.Content["key1"]
 	assert.True(t, ok)
 	assert.Equal(t, val, "value1")
 
-	val, ok = ev.content["key2"]
+	val, ok = ev.Content["key2"]
 	assert.True(t, ok)
 	assert.Equal(t, val, "value2")
 }
