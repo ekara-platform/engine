d36e008be2d312614e958b2bfb07c2879fffee48
nirekin <nirekin@hotmail.com>
2019-04-26 13:48:32 +0200

    Refactor SCM handlers

---
+++ component/scm/fetched_component.go
@@ -0,0 +1,31 @@
+package scm
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ekara-platform/model"
+)
+
+type (
+
+	//FetchedComponent represents a component fetched locally
+	FetchedComponent struct {
+		ID            string
+		LocalPath     string
+		Descriptor    string
+		DescriptorUrl model.EkUrl
+		LocalUrl      model.EkUrl
+	}
+)
+
+func (fc FetchedComponent) String() string {
+	return fmt.Sprintf("id: %s, localPath: %s, descriptor: %s", fc.ID, fc.LocalPath, fc.Descriptor)
+}
+
+func (fc FetchedComponent) HasDescriptor() bool {
+	if _, err := os.Stat(fc.DescriptorUrl.AsFilePath()); err == nil {
+		return true
+	}
+	return false
+}
---
+++ component/scm/file/doc.go
@@ -0,0 +1,8 @@
+// Package file implements the git connector used to fetch components from local repositories.
+//
+// It is basically an implementation of the interface "scmHandler"
+// in "github.com/ekara-platform/engine/component/scm"
+//
+// Currently the local repositories are only used for testing purpose but later the should be
+// used as part of an environment descriptor.
+package file
