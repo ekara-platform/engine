0be6b32d99589db30db71f6538ff610793384b9a
Adrien LAUER <adrien.lauer@gmail.com>
2019-08-14 15:55:58 +0200

    Fix deployment parent

--- step_deploy.go
+++ step_deploy.go
@@ -11,25 +11,26 @@ import (
 
 var deploySteps = []step{fstack}
 
+// TODO: refactor this as most of playbook/compose deployment is the same
 func fstack(lC LaunchContext, rC *runtimeContext) StepResults {
 	cm := lC.Ekara().ComponentManager()
 	sCs := InitStepResults()
 	for _, st := range cm.Environment().Stacks {
-		sc := InitCodeStepResult("Starting a stack setup phase", st, NoCleanUpRequired)
-		lC.Log().Printf("Checking how to install %s", st.Name)
+		sc := InitCodeStepResult("Deploying stack", st, NoCleanUpRequired)
+		lC.Log().Printf("Checking how to deploy stack %s", st.Name)
 		sCs.Add(sc)
 
+		// Make the stack usable
 		ust, err := cm.Use(st)
 		if err != nil {
 			FailsOnCode(&sc, err, "An error occurred getting the usable stack", nil)
 		}
 		defer ust.Release()
+
 		if ok, _ := ust.ContainsFile("install.yml"); ok {
 			fstackPlaybook(lC, rC, st, ust, sCs)
 		} else {
-			//fstackCompose(lC, rC, lC.Ekara().ComponentManager().Environment().Ekara.Parent, st, sCs)
-			// TODO delete the unused components
-			fstackCompose(lC, rC, model.Parent{}, st, sCs)
+			fstackCompose(lC, rC, st, ust, sCs)
 		}
 	}
 	return *sCs
@@ -37,7 +38,7 @@ func fstack(lC LaunchContext, rC *runtimeContext) StepResults {
 
 func fstackPlaybook(lC LaunchContext, rC *runtimeContext, st model.Stack, ust component.UsableComponent, sCs *StepResults) {
 	cm := lC.Ekara().ComponentManager()
-	sc := InitPlaybookStepResult("Running the stack playbook installation phase", st, NoCleanUpRequired)
+	sc := InitPlaybookStepResult("Deploying stack with a custom playbook", st, NoCleanUpRequired)
 
 	// Stack install exchange folder for the given provider
 	fName := fmt.Sprintf("install_stack_%s", st.Name)
@@ -128,15 +129,12 @@ func fstackPlaybook(lC LaunchContext, rC *runtimeContext, st model.Stack, ust co
 }
 
 // TODO currently only the parent is able to deploy a compose...
-// TODO refactor this method in order to run the docker stack deploy only once for the whole cluster,
-// regardless of the number of providers...
-func fstackCompose(lC LaunchContext, rC *runtimeContext, parent model.Parent, s model.Stack, sCs *StepResults) {
+func fstackCompose(lC LaunchContext, rC *runtimeContext, st model.Stack, ust component.UsableComponent, sCs *StepResults) {
 	cm := lC.Ekara().ComponentManager()
-
-	sc := InitPlaybookStepResult("Running the stack compose installation phase", s, NoCleanUpRequired)
+	sc := InitPlaybookStepResult("Deploying stack with a Docker compose", st, NoCleanUpRequired)
 
 	// Stack install exchange folder for the given provider
-	fName := fmt.Sprintf("install_stack_%s", s.Name)
+	fName := fmt.Sprintf("install_stack_%s", st.Name)
 
 	stackEf, ko := createChildExchangeFolder(lC.Ef().Input, fName, &sc, lC.Log())
 	if ko {
@@ -149,7 +147,7 @@ func fstackCompose(lC LaunchContext, rC *runtimeContext, parent model.Parent, s 
 
 	// Prepare parameters
 	bp := BuildBaseParam(lC, "")
-	bp.AddNamedMap("params", s.Parameters)
+	bp.AddNamedMap("params", st.Parameters)
 	if ko := saveBaseParams(bp, lC, stackEf.Input, &sc); ko {
 		sCs.Add(sc)
 		return
@@ -161,7 +159,7 @@ func fstackCompose(lC LaunchContext, rC *runtimeContext, parent model.Parent, s 
 	env.AddProxy(lC.Proxy())
 
 	// Adding the environment variables from the stack
-	for envK, envV := range s.EnvVars {
+	for envK, envV := range st.EnvVars {
 		env.Add(envK, envV)
 	}
 
@@ -171,7 +169,7 @@ func fstackCompose(lC LaunchContext, rC *runtimeContext, parent model.Parent, s 
 		rC,
 		sCs,
 		cm.Environment().Hooks.Deploy,
-		hookContext{"install", s, "environment", "deploy", bp, env, buffer},
+		hookContext{"install", st, "environment", "deploy", bp, env, buffer},
 		NoCleanUpRequired,
 	)
 
@@ -180,35 +178,35 @@ func fstackCompose(lC LaunchContext, rC *runtimeContext, parent model.Parent, s 
 		lC,
 		rC,
 		sCs,
-		s.Hooks.Deploy,
-		hookContext{"install", s, "stack", "deploy", bp, env, buffer},
+		st.Hooks.Deploy,
+		hookContext{"install", st, "stack", "deploy", bp, env, buffer},
 		NoCleanUpRequired,
 	)
 
 	// Prepare extra vars
 	var exv ansible.ExtraVars
 
-	d, err := cm.Use(parent)
+	// Make parent usable
+	parent, err := cm.Use(cm.Environment().Platform().Parent)
 	if err != nil {
 		FailsOnCode(&sc, err, "An error occurred getting the usable parent", nil)
 	}
-	defer d.Release()
-	su, err := cm.Use(s)
-	if err != nil {
-		FailsOnCode(&sc, err, "An error occurred getting the usable stack", nil)
-	}
-	defer su.Release()
-	if ok, match := su.ContainsFile("docker-compose.yml"); ok {
-		exv = ansible.BuildExtraVars("compose_path="+filepath.Join(match.Component().RootPath(), match.RelativePath()), *stackEf.Input, *stackEf.Output, buffer)
+	defer parent.Release()
+	if ok, match := ust.ContainsFile("docker-compose.yml"); ok {
+		exv = ansible.BuildExtraVars(
+			fmt.Sprintf("compose_path=%s stack_name=%s", filepath.Join(match.Component().RootPath(), match.RelativePath()), st.Name),
+			*stackEf.Input,
+			*stackEf.Output,
+			buffer)
 	} else {
-		exv = ansible.BuildExtraVars("", *stackEf.Input, *stackEf.Output, buffer)
+		FailsOnCode(&sc, err, fmt.Sprintf("Stack %s neither have an install.yml playbook nor a docker-compose.yml file", st.Name), nil)
 	}
 
-	code, err := lC.Ekara().AnsibleManager().Execute(d, "deploy_compose.yaml", exv, env)
+	code, err := lC.Ekara().AnsibleManager().Execute(parent, "deploy_compose.yaml", exv, env)
 	if err != nil {
 		pfd := playBookFailureDetail{
 			Playbook:  "deploy_compose.yaml",
-			Component: parent.ComponentName(),
+			Component: parent.Name(),
 			Code:      code,
 		}
 		FailsOnPlaybook(&sc, err, "An error occurred executing the playbook", pfd)
@@ -221,8 +219,8 @@ func fstackCompose(lC LaunchContext, rC *runtimeContext, parent model.Parent, s 
 		lC,
 		rC,
 		sCs,
-		s.Hooks.Deploy,
-		hookContext{"install", s, "stack", "deploy", bp, env, buffer},
+		st.Hooks.Deploy,
+		hookContext{"install", st, "stack", "deploy", bp, env, buffer},
 		NoCleanUpRequired,
 	)
 
@@ -232,7 +230,7 @@ func fstackCompose(lC LaunchContext, rC *runtimeContext, parent model.Parent, s 
 		rC,
 		sCs,
 		cm.Environment().Hooks.Deploy,
-		hookContext{"install", s, "environment", "deploy", bp, env, buffer},
+		hookContext{"install", st, "environment", "deploy", bp, env, buffer},
 		NoCleanUpRequired,
 	)
 
